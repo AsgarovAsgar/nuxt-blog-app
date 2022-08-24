9913da3d97b3a044e2469467c00fcfa435de6d98
Asgarov Asgar <asgara@maestro.st>
2022-08-24 23:42:26 +0400

    added temporary auth system and middleware

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -1,10 +1,44 @@
 <template>
-  <div>auth</div>
+  <div>
+    <div>
+      <form @submit.prevent="onSubmit()" class="border p-4 flex flex-col gap-4 max-w-md">
+        <div class="flex flex-col gap-2">
+          <label for="email">Email</label>
+          <input v-model="email" type="email" name="" id="email">
+        </div>
+        <div class="flex flex-col gap-2">
+          <label for="password">Password</label>
+          <input v-model="password" type="password" name="" id="password">
+        </div>
+        <button class="border px-3 py-2 rounded" type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</button>
+        <button class="border px-3 py-2 rounded" type="button" @click="isLogin = !isLogin">Switch to {{ isLogin ? 'Sign Up' : 'Login' }}</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   layout: 'admin',
+  data() {
+    return {
+      isLogin: true,
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$store.dispatch('authenticateUser', {
+        isLogin: this.isLogin,
+        email: this.email,
+        password: this.password
+      })
+      .then(() => {
+        this.$router.push('/admin')
+      })
+    }
+  }
 }
 </script>
 
--- store/index.js
+++ store/index.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 export const state = () => ({
-    loadedPosts: []
+    loadedPosts: [],
+    token: null
   })
   
 export const mutations = {
@@ -16,12 +17,18 @@ export const mutations = {
         post => post.id === editedPost.id
       );
       state.loadedPosts[postIndex] = editedPost
+    },
+    setToken(state, idToken) {
+      state.token = idToken
+    },
+    clearToken(state) {
+      state.token = null
     }
 }
   
   export const actions = {
     nuxtServerInit(vuexContext, context) {
-      console.log('context', context.$config);
+      // console.log('context', context.$config);
       return context.$axios.$get('/posts.json')
       // if we call data using $axios.$get we use full functionality of axios module, so we don't need to write baseUrl
       .then(data => {
@@ -43,7 +50,7 @@ export const mutations = {
         ...post,
         updatedDate: new Date()
       }
-      return this.axios.post(this.$config.axios.baseURL + '/posts.json', createdPost)
+      return this.axios.post(this.$config.axios.browserBaseURL + '/posts.json', createdPost)
       .then(result => {
         vuexContext.commit('addPost', {...createdPost, id: result.data.name })
         // this.$router.push('/admin')
@@ -52,17 +59,47 @@ export const mutations = {
       .catch(err => console.log(err))
     },
     editPost(vuexContext, editedPost) {
-      return this.$axios.put(this.$config.axios.baseURL + editedPost.id + '.json', editedPost)
+      // console.log('this.$config.axios.baseURL', this.$config);
+      // console.log('editedPost.id', editedPost.id);
+      return this.$axios.put(this.$config.baseUrl + '/posts/' + editedPost.id + '.json' + '?auth=' + vuexContext.state.token, editedPost)
       // this is modern way to call axios. just add base url into the runtimeconfig.axios object 
       .then(res => {
+        // console.log('res, res', res);
         vuexContext.commit('editPost', editedPost)
       })
       .catch(err => console.log(err))
+    },
+    authenticateUser(vuexContext, authData) {
+      let authEndpoint = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='
+      if(authData.isLogin) {
+        authEndpoint = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='
+      }
+      return this.$axios.$post(authEndpoint + this.$config.fbWebApiKey, {
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true
+      })
+      .then(result => {
+        console.log('result: ', result);
+        vuexContext.commit('setToken', result.idToken)
+        vuexContext.dispatch('setLogoutTimer', result.expiresIn * 1000)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    },
+    setLogoutTimer(vuexContext, duration) {
+      setTimeout(() => {
+        vuexContext.commit('clearToken')
+      }, duration);
     }
   }
 
   export const getters = {
     loadedPosts(state) {
-        return state.loadedPosts
+      return state.loadedPosts
+    },
+    isAuthenticated(state) {
+      return state.token != null
     }
   }
